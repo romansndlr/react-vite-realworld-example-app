08b11b3ecd3f4fb377c6be6162d59489a247aedd
Roman Sandler <roman.sandler@fiverr.com>
2021-07-14 10:25:13 +0300

    wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,8 @@
-import classNames from 'classnames'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import faker from 'faker'
 import React from 'react'
-import { ArticleList, PopularTags } from '../components'
-import { useAuth } from '../hooks'
-
-/**
- * @type {object} Filters
- * @property {string} Filter.tag
- */
-const initialFilters = { tag: '' }
 
 function Home() {
-  const { isAuth } = useAuth()
-  const [filters, setFilters] = React.useState(initialFilters)
-  const [isFeed, setIsFeed] = React.useState(false)
-
-  React.useEffect(() => {
-    setIsFeed(isAuth)
-  }, [isAuth])
-
-  function onTagClick(tag) {
-    setFilters((prevFilters) => ({ ...prevFilters, tag }))
-    setIsFeed(false)
-  }
-
-  function onGlobalFeedClick() {
-    setFilters(initialFilters)
-    setIsFeed(false)
-  }
-
   return (
     <div className="home-page">
       <div className="banner">
@@ -41,41 +16,84 @@ function Home() {
           <div className="col-md-9">
             <div className="feed-toggle">
               <ul className="nav nav-pills outline-active">
-                {isAuth && (
-                  <li className="nav-item">
-                    <button
-                      onClick={() => setIsFeed(true)}
-                      type="button"
-                      className={classNames('nav-link', {
-                        active: isFeed,
-                      })}
-                    >
-                      Your Feed
-                    </button>
-                  </li>
-                )}
                 <li className="nav-item">
                   <button
                     type="button"
-                    className={classNames('nav-link', {
-                      active: !filters?.tag && !isFeed,
-                    })}
-                    onClick={onGlobalFeedClick}
+                    className="nav-link" // Add active when feed
+                  >
+                    Your Feed
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button
+                    type="button"
+                    className="nav-link" // Add active if not feed and not tag
                   >
                     Global Feed
                   </button>
                 </li>
-                {filters?.tag && (
-                  <li className="nav-item">
-                    <a className="nav-link active"># {filters?.tag}</a>
-                  </li>
-                )}
+                <li className="nav-item">
+                  <a className="nav-link active"># {faker.lorem.word()}</a>
+                </li>
               </ul>
             </div>
-            <ArticleList isFeed={isFeed} filters={filters} />
+            <div className="article-preview">
+              <div className="article-meta">
+                <a>
+                  <img src={faker.image.avatar()} />
+                </a>
+                <div className="info">
+                  <a className="author">{faker.internet.userName()}</a>
+                  <span className="date">{new Date(faker.date.past()).toDateString()}</span>
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-sm btn-outline-primary" // Change to btn-primary if favorited
+                  disabled={false}
+                >
+                  <i className="ion-heart" />
+                  &nbsp; {faker.datatype.number()}
+                </button>
+              </div>
+              <a className="preview-link">
+                <h1>{faker.lorem.sentence()}</h1>
+                <p>{faker.lorem.paragraph()}</p>
+                <span>Read more...</span>
+                <ul className="tag-list">
+                  <li className="tag-default tag-pill tag-outline">{faker.lorem.word()}</li>
+                </ul>
+              </a>
+            </div>
+            <nav>
+              <ul className="pagination">
+                <li className="page-item">
+                  <button type="button" className="page-link">
+                    1
+                  </button>
+                </li>
+                <li className="page-item">
+                  <button type="button" className="page-link">
+                    2
+                  </button>
+                </li>
+                <li className="page-item active">
+                  <button type="button" className="page-link">
+                    3
+                  </button>
+                </li>
+              </ul>
+            </nav>
           </div>
           <div className="col-md-3">
-            <PopularTags onTagClick={onTagClick} />
+            <a href="#" className="tag-pill tag-default">
+              {faker.lorem.word()}
+            </a>
+            <a href="#" className="tag-pill tag-default">
+              {faker.lorem.word()}
+            </a>
+            <a href="#" className="tag-pill tag-default">
+              {faker.lorem.word()}
+            </a>
           </div>
         </div>
       </div>
