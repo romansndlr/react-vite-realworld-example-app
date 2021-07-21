c3db97f4836f4385bf2f4157357852fc4e218d8c
Alon Wolenitz <alonsukasa@gmail.com>
2021-07-21 19:32:02 +0300

    Separated to hooks, models and comps and added Mutation for favorite

---
+++ src/models/Article.js
@@ -0,0 +1,28 @@
+import { Author } from '.'
+class Article {
+  constructor({
+    slug = '',
+    title = '',
+    description = '',
+    body = '',
+    tagList = [],
+    createdAt = '',
+    updatedAt = '',
+    favorited = false,
+    favoritesCount = 0,
+    author = {},
+  }) {
+    this.slug = slug
+    this.title = title
+    this.description = description
+    this.body = body
+    this.tagList = tagList
+    this.createdAt = new Date(createdAt).toDateString()
+    this.updatedAt = new Date(updatedAt).toDateString()
+    this.favorited = favorited
+    this.favoritesCount = favoritesCount
+    this.author = new Author(author)
+  }
+}
+
+export default Article
