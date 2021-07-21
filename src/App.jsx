a1f87e3a308ba2fd05d9288a4601bdaa51926246
Alon Palgy <a.palgy@gmail.com>
2021-07-21 11:06:50 +0300

    WIP, currently working on pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Article, Home } from './pages'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import './App.css'
 
