28dea240472ca9ac40c284fe76eb2e1754962460
Amit Yizraeli <amit-yiz@noal.org.il>
2021-09-05 12:01:32 +0300

    class work

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -11,8 +11,46 @@ afterEach(() => {
   server.shutdown()
 })
 
-it('should render a list of articles', () => {})
+it('should render a list of articles', () => {
+  /* ARRANGE */
+  const user = server.create('user')
+  const article1 = server.create('article', { author: user })
+  const article2 = server.create('article', { author: user })
+  const article3 = server.create('article', { author: user })
 
-it('should filter the list of articles by tag', () => {})
+  /* ACT */
+  cy.visit('/')
+
+  /* ASSERT */
+  cy.findByRole('heading', { name: article1.title }).should('exist')
+  cy.findByRole('heading', { name: article2.title }).should('exist')
+  cy.findByRole('heading', { name: article3.title }).should('exist')
+  cy.findByText(article1.body).should('exist')
+  cy.findByText(article2.body).should('exist')
+  cy.findByText(article3.body).should('exist')
+})
+
+it.only('should filter the list of articles by tag', () => {
+  /* ARRANGE */
+  const user = server.create('user')
+  const tag = server.create('tag')
+  const article1 = server.create('article', { author: user })
+  const article2 = server.create('article', { author: user })
+  const article3 = server.create('article', { author: user })
+  article1.update({ tagsList: [tag.text] })
+  article2.update({ tagsList: [] })
+  article3.update({ tagsList: [] })
+
+  /* ACT */
+  cy.visit('/')
+  cy.findByTestId('popular-tags').within(() => {
+    cy.findByRole('link', { name: tag.text }).click()
+  })
+
+  /* ASSERT */
+  cy.findByRole('heading', { name: article1.title }).should('exist')
+  cy.findByRole('heading', { name: article2.title }).should('not.exist')
+  cy.findByRole('heading', { name: article3.title }).should('not.exist')
+})
 
 it('should move between pages', () => {})
