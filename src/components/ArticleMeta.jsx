a6396261eb214bfefb48e2053a773bf0a0af7c20
alonisser <alonisser@gmail.com>
2021-10-01 21:44:28 +0300

    article meta changes integrating the follow button, the mutation config

--- src/components/ArticleMeta.jsx
+++ src/components/ArticleMeta.jsx
@@ -2,33 +2,64 @@ import React from 'react'
 import { useAuth } from '../hooks'
 import ArticleHeadline from './ArticleHeadline'
 import FavoriteArticleButton from './FavoriteArticleButton'
+import FollowUserButton from './FollowUserButton'
+import { useQueryClient } from 'react-query'
+
 
 function ArticleMeta({ article }) {
   const { authUser } = useAuth()
+  const queryClient = useQueryClient()
+  const queryKey = `/articles/${article.slug}`
+
+  const followMutationConfiguration = {
+    onMutate: async () => {
+      await queryClient.cancelQueries(queryKey)
+
+      const previousArticle = queryClient.getQueryData(queryKey)
+
+      queryClient.setQueryData(queryKey, ({ article }) => ({
+        article: {
+          ...article,
+          author: {
+            ...article.author,
+            following: !article.author.following
+          }
+
+        }
+      }))
+
+      return { previousArticle }
+    },
+    onError: (err, newTodo, context) => {
+      queryClient.setQueryData(queryKey, context.previousArticle)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(queryKey)
+      queryClient.invalidateQueries({
+        predicate: (query) => {
+          return query.queryKey[0].startsWith('/articles')
+        }
+      })
+    }
+  }
   return (
-    <div className="article-meta">
+    <div className='article-meta'>
       <ArticleHeadline article={article} />
       <span>
         {authUser.username === article.author?.username ? (
           <>
-            <a className="btn btn-outline-secondary btn-sm">
-              <i className="ion-edit" /> Edit Article
+            <a className='btn btn-outline-secondary btn-sm'>
+              <i className='ion-edit' /> Edit Article
             </a>
             &nbsp;&nbsp;
-            <button disabled={false} type="button" className="btn btn-outline-danger btn-sm">
-              <i className="ion-trash-a" /> Delete Article
+            <button disabled={false} type='button' className='btn btn-outline-danger btn-sm'>
+              <i className='ion-trash-a' /> Delete Article
             </button>
           </>
         ) : (
           <>
-            <button
-              disabled={false}
-              type="button"
-              className="btn btn-sm action-btn btn-outline-secondary " // Change to btn-secondary if following
-            >
-              <i className="ion-plus-round" />
-              &nbsp; Follow {article.author?.username} {/* Change to Unfollow if following */}
-            </button>
+            <FollowUserButton user={article.author}
+                              followMutationConfiguration={followMutationConfiguration}></FollowUserButton>
             &nbsp;&nbsp;
             <FavoriteArticleButton article={article}>Favorite Article</FavoriteArticleButton>
           </>
