72cc2414b47edf698f9931f8cf96f1e16c2e5831
Alon Palgy <a.palgy@gmail.com>
2021-10-02 17:15:05 +0300

    following button

--- src/components/PopularTags.jsx
+++ src/components/PopularTags.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import { useQuery } from 'react-query'
+import axios from 'axios'
 
 function PopularTags({ onClick }) {
-  const { data, isFetching } = useQuery('/tags', {
-    placeholderData: {
-      tags: [],
+  const { data, isFetching } = useQuery('tags',
+    async () => {
+      const { data } = await axios.get(`/tags`)
+      return data
     },
+    {
+    placeholderData: {
+      tags: []
+    }
   })
 
   return (
-    <div className="sidebar" data-testid="popular-tags">
+    <div className='sidebar' data-testid='popular-tags'>
       <p>Popular Tags</p>
       {isFetching && <div>Loading tags...</div>}
-      {data?.tags.map((tag) => (
-        <a
-          key={tag}
-          onClick={(e) => {
-            e.preventDefault()
-            onClick(tag)
-          }}
-          href=""
-          className="tag-pill tag-default"
-        >
-          {tag}
-        </a>
-      ))}
+      {data?.tags && [...new Set(data.tags)].map((tag) => (
+          <a
+            key={tag}
+            onClick={(e) => {
+              e.preventDefault()
+              onClick(tag)
+            }}
+            href=''
+            className='tag-pill tag-default'
+          >
+            {tag}
+          </a>
+        )
+      )}
     </div>
   )
 }
