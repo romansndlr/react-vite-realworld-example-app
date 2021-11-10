e804ce5096a94e94810ab6ac354bb32c9e67f7d0
Doron Gescheidt <dgescheidt3@gmail.com>
2021-11-10 12:41:19 +0200

    add settings page

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,37 +1,62 @@
 import React from 'react'
+import { Formik, Form, Field } from 'formik'
+import { useAuth } from '../hooks'
+import axios from 'axios'
 
 function SettingsPage() {
+  const { authUser } = useAuth()
+  console.log(authUser)
   return (
-    <div className="settings-page">
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Your Settings</h1>
+    <div className='settings-page'>
+      <div className='container page'>
+        <div className='row'>
+          <div className='col-md-6 offset-md-3 col-xs-12'>
+            <h1 className='text-xs-center'>Your Settings</h1>
+            <Formik
+              initialValues={{
+                image: authUser.image,
+                username: authUser.username,
+                bio: authUser.bio,
+                email: authUser.email
+              }}
+              onSubmit={async (values) => {
+                console.log(values)
+                await axios.put('/user', { user: values })
 
-            <form>
-              <fieldset>
-                <fieldset className="form-group">
-                  <input className="form-control" type="text" placeholder="URL of profile picture" />
+              }}
+
+            >
+              <Form>
+                <fieldset>
+                  <fieldset className='form-group'>
+                    <Field className='form-control' type='text' placeholder='URL of profile picture' name='image'
+                           id='image' />
+                  </fieldset>
+                  <fieldset className='form-group'>
+                    <Field className='form-control form-control-lg' type='text' placeholder='Your Name' name='username'
+                           id='username' />
+                  </fieldset>
+                  <fieldset className='form-group'>
+                    <Field
+                      as='textarea'
+                      className='form-control form-control-lg'
+                      rows={8}
+                      placeholder='Short bio about you'
+                      name='bio'
+                    ></Field>
+                  </fieldset>
+                  <fieldset className='form-group'>
+                    <Field className='form-control form-control-lg' type='text' placeholder='Email' name='email' />
+                  </fieldset>
+                  <fieldset className='form-group'>
+                    <input className='form-control form-control-lg' type='password' placeholder='Password'
+                           name='password' />
+                  </fieldset>
+                  <button className='btn btn-lg btn-primary pull-xs-right'>Update Settings</button>
                 </fieldset>
-                <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Your Name" />
-                </fieldset>
-                <fieldset className="form-group">
-                  <textarea
-                    className="form-control form-control-lg"
-                    rows={8}
-                    placeholder="Short bio about you"
-                  ></textarea>
-                </fieldset>
-                <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Email" />
-                </fieldset>
-                <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="password" placeholder="Password" />
-                </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right">Update Settings</button>
-              </fieldset>
-            </form>
+              </Form>
+            </Formik>
+
           </div>
         </div>
       </div>
